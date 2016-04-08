3edff46a4d5d623c7fafee036c5678fd3cae1716
Sykie Chen <sykiechen@gmail.com>
2016-04-08 16:56:59 +0800

    mainly done

--- PiePick/main.cpp
+++ PiePick/main.cpp
@@ -62,8 +62,8 @@ public:
 class mainForm {
 private:
 	wchar_t username[20];
-	IMAGE background;
-	int highScore = 0;
+	IMAGE* background;
+	int highScore = -999;
 	int score = 0;
 	int time = 200;
 	int life = 3;
@@ -76,13 +76,22 @@ public:
 		highScore = hs;
 		highScoreBox.setValue(hs);
 	}
+	int getHighScore() {
+		return highScore;
+	}
 	void setScore(int s) {
 		score = s;
 		scoreBox.setValue(s);
 	}
+	int getScore() {
+		return score;
+	}
 	void setLife(int l) {
 		life = l;
 		lifeBox.setValue(l);
+	}
+	int getLife() {
+		return life;
 	}
 	void setTime(int time) {
 		this->time = time;
@@ -93,11 +102,11 @@ public:
 	}
 
 	//text boxes to be rewritten as a class
-	void loadBackground(LPCTSTR bgPath) {
-		loadimage(&background, bgPath);
+	void setBackground(IMAGE* bg) {
+		background = bg;
 	}
 	void refreshBackground() {
-		putimage(0, 0, &background);
+		putimage(0, 0, background);
 	}
 	void setUsername(LPCTSTR name) {
 		wcscpy_s(username, name);
@@ -109,9 +118,7 @@ public:
 		swprintf_s(sName, L"Player: %s", username);
 		drawtext(sName, &rName, DT_LEFT | DT_TOP);
 	}
-
-	void init() {
-		initgraph(800, 600);
+	void refresh() {
 		//paint bg
 		refreshBackground();
 		showUsername();
@@ -131,6 +138,10 @@ public:
 		lifeBox.setValue(life);
 		lifeBox.setPos(200, 10, 350, 40);
 		lifeBox.show();
+	}
+	void init() {
+		initgraph(800, 600);
+		refresh();
 	}
 };
 
@@ -175,6 +186,7 @@ private:
 	IMAGE background;
 	IMAGE backgroundX;
 	IMAGE backupBg; //bg's backup
+	IMAGE* mainBg; //main background
 	int x;
 	int y;
 	int height;
@@ -182,6 +194,9 @@ private:
 	int bkX;
 	int bkY;
 public:
+	void setMainBg(IMAGE* bg) {
+		mainBg = bg;
+	}
 	void loadBg(LPCTSTR bgPath, LPCTSTR bgXPath) {
 		loadimage(&background, bgPath);
 		height = background.getheight();
@@ -200,7 +215,10 @@ public:
 	}
 	void show() {
 		//back up the background
+		IMAGE *workImg = GetWorkingImage();
+		SetWorkingImage(mainBg);
 		getimage(&backupBg, x, y, width, height);
+		SetWorkingImage(workImg);
 		transimg(x, y, &background, &backgroundX);
 		bkX = x;
 		bkY = y;
@@ -217,6 +235,14 @@ public:
 			refresh();
 		}
 	}
+	void moveU(int step) {
+		y -= step;
+		refresh();
+	}
+	void moveD(int step) {
+		y += step;
+		refresh();
+	}
 	void refresh() {
 		BeginBatchDraw();
 		putimage(bkX, bkY, &backupBg);
@@ -225,6 +251,9 @@ public:
 	}
 	int getX() {
 		return x;
+	}
+	int getY() {
+		return y;
 	}
 };
 
@@ -239,10 +268,26 @@ private:
 	IMAGE unknown;
 	IMAGE unknownX;
 	IMAGE backupBg; //bg's backup
+	IMAGE mainBg;
 	int x, y, width, height;
 	int bkX, bkY;
+	clock_t timeMark;
 public:
-	ball() {
+	/*ball(IMAGE* bg) {
+		mainBg = bg;
+		loadimage(&pie, L"img\\pie.bmp");
+		loadimage(&pieX, L"img\\piex.bmp");
+		loadimage(&bomb, L"img\\bomb.bmp");
+		loadimage(&bombX, L"img\\bombx.bmp");
+		loadimage(&unknown, L"img\\unknown.bmp");
+		loadimage(&unknownX, L"img\\unknownx.bmp");
+		height = unknown.getheight();
+		width = unknown.getwidth();
+		y = 40;
+
+	}*/
+	void init() {
+		loadimage(&mainBg, L"img\\background.jpg");
 		loadimage(&pie, L"img\\pie.bmp");
 		loadimage(&pieX, L"img\\piex.bmp");
 		loadimage(&bomb, L"img\\bomb.bmp");
@@ -253,14 +298,27 @@ public:
 		width = unknown.getwidth();
 		y = 40;
 	}
+	void setTimeMark(clock_t time) {
+		timeMark = time;
+	}
+	clock_t getTimeMark() {
+		return timeMark;
+	}
 	void setX(int x) {
 		this->x = x;
 	}
 	void setAsPie(bool ifPie) {
 		flgIsPie = ifPie;
 	}
+	/*void setMainBg(IMAGE* bg) {
+		mainBg = bg;
+	}*/
 	void show() {
+		//back up the background
+		IMAGE *workImg = GetWorkingImage();
+		SetWorkingImage(&mainBg);
 		getimage(&backupBg, x, y, width, height);
+		SetWorkingImage(workImg);
 		if (flgIsVisible) {
 			if (flgIsPie)
 				transimg(x, y, &pie, &pieX);
@@ -279,14 +337,28 @@ public:
 		show();
 		EndBatchDraw();
 	}
+	void hide() {
+		BeginBatchDraw();
+		putimage(bkX, bkY, &backupBg);
+		EndBatchDraw();
+	}
 	void uncover() {
 		flgIsVisible = true;
 	}
 	void cover() {
 		flgIsVisible = false;
 	}
+	bool isVisible() {
+		return flgIsVisible;
+	}
 	int getX() {
 		return x;
+	}
+	int getY() {
+		return y;
+	}
+	int getWidth() {
+		return width;
 	}
 	void moveDown(int step) {
 		y += step;
@@ -298,9 +370,16 @@ public:
 
 };
 
-
+struct ballNode {
+	ball data;
+	ballNode* next;
+	//ballNode* previous;
+};
 
 int main() {
+	srand((unsigned)time(NULL));
+
+
 	/*initgraph(800, 600);
 	// Load background
 	IMAGE background;
@@ -335,7 +414,9 @@ int main() {
 	wchar_t usrName[20] = L"";
 	while (!InputBox(usrName, 20, L"Please input your name:", L"Input name") || !wcscmp(usrName, L""));
 	mainForm mForm;
-	mForm.loadBackground(L"img\\background.jpg");
+	IMAGE bgImg;
+	loadimage(&bgImg, L"img\\background.jpg");
+	mForm.setBackground(&bgImg);
 	mForm.setUsername(usrName);
 	mForm.init();
 	//load buttons
@@ -354,6 +435,7 @@ int main() {
 	//load bow
 	bow mario;
 	mario.loadBg(L"img\\mario.bmp", L"img\\mariox.bmp");
+	mario.setMainBg(&bgImg);
 	mario.setPos(400, 465);
 	mario.show();
 
@@ -381,14 +463,28 @@ int main() {
 	}
 
 	*/
+	//form list
+	ballNode* ballHead;
+	ballHead = new ballNode;
+	//ballHead->previous = NULL;
+	ballHead->next = NULL;
+	ballNode* ballLast;
+	ballLast = ballHead;
+
+
 
 	//main loop
 	bool flgEsc = false;
 	bool flgPlay = false;
 	bool flgKeyL = false;
 	bool flgKeyR = false;
+	bool flgGameOver = false;
+	bool flgDead = false;
+	clock_t t1 = 700;
 	clock_t timeKey = clock();
 	clock_t timeGame = 0;
+	clock_t timeBallMove = clock();
+	clock_t timeBallMake = clock();
 	MOUSEMSG mouseMsg = GetMouseMsg();
 	while (!flgEsc) {
 		// check mouse
@@ -408,17 +504,24 @@ int main() {
 					mForm.scoreBox.refresh();
 				}
 				else if (btStop.chkRange(mouseMsg.x, mouseMsg.y)) {
+					timeGame = 0;
+					mForm.setTime(200);
+					mForm.setLife(3);
+					mForm.setScore(0);
 					flgPlay = false;
-					mario.setPos(400, 465);
-					mario.refresh();
+					flgGameOver = true;
 				}
 			}
 		}
-		if (flgPlay) { //if in game
+		
+
+
+
+		if (flgPlay && !flgGameOver) { //if in game
 			//check keyboard
 			if (_kbhit()) {
 				//slow down the response
-				if (((clock() - timeKey)) > 1) {
+				if (((clock() - timeKey)) > 3) {
 					timeKey = clock();
 					if (GetAsyncKeyState(VK_LEFT) & 0x8000)
 						mario.moveL(2);
@@ -433,17 +536,177 @@ int main() {
 				mForm.setTime(mForm.getTime() - 1);
 				mForm.timeBox.refresh();
 				if (mForm.getTime() == 0) {
-					//reset game
+
+					//chk high score
+					if (mForm.getScore() > mForm.getHighScore()) {
+						mForm.setHighScore(mForm.getScore());
+						mForm.highScoreBox.refresh();
+					}
+
+					timeGame = 0;
+					mForm.setTime(200);
+					mForm.setLife(3);
+					mForm.setScore(0);
 					flgPlay = false;
-					mario.setPos(400, 465);
-					mario.refresh();
+					flgGameOver = true;
 				}
 			}
+			
+			//balls
+			//new ball
+			if (clock()-timeBallMake >=2000) { //every 2 sec a ball
+				timeBallMake = clock();
+				ballLast->next = new ballNode;
+				ballLast = ballLast->next;
+				ballLast->next = NULL;
+				ballLast->data.init();
+				if (rand() % 3 == 0) {
+					// 0.33 to be a bomb
+					ballLast->data.setAsPie(false);
+				}
+				else {
+					//0.66 pie
+					ballLast->data.setAsPie(true);
+				}
+				ballLast->data.setX(rand() % 750);
+				ballLast->data.setTimeMark(clock());
+				//test
+				//ballLast->data.uncover();
+
+				ballLast->data.show();
+			}
+			
+
+			//move existing balls
+			if (clock() - timeBallMove > 50) {
+				timeBallMove = clock();
+				ballNode* p = ballHead->next;
+				while (p != NULL) {
+					p->data.moveDown(3);
+					p->data.refresh();
+					//flash existing balls
+					if (clock() - p->data.getTimeMark() > t1 && !p->data.isVisible()) {
+						p->data.uncover();
+						p->data.setTimeMark(clock());
+					}
+					else if (clock() - p->data.getTimeMark() > 200 && p->data.isVisible()) {
+						p->data.cover();
+						p->data.setTimeMark(clock());
+					}
+
+
+					p = p->next;
+				}
+				//hit bottom
+				if (ballHead->next != NULL) { //prevent the first overflow
+					if (ballHead->next->data.getY() > 409) {
+						if (mario.getX() > ballHead->next->data.getX() - ballHead->next->data.getWidth() && mario.getX() < ballHead->next->data.getX() + ballHead->next->data.getWidth()) {
+							//catched
+							if (ballHead->next->data.isPie()) {
+								//catched pie
+								mForm.setScore(mForm.getScore() + 10);
+								mForm.scoreBox.refresh();
+							}
+							else {
+								//catched bomb
+								mForm.setScore(mForm.getScore() - 100);
+								mForm.scoreBox.refresh();
+								mForm.setLife(mForm.getLife() - 1);
+								mForm.lifeBox.refresh();
+								flgDead = true;
+								if (mForm.getLife() == 0) {
+									//dead
+
+									//chk high score
+									if (mForm.getScore() > mForm.getHighScore()) {
+										mForm.setHighScore(mForm.getScore());
+										mForm.highScoreBox.refresh();
+									}
+
+									timeGame = 0;
+									mForm.setTime(200);
+									mForm.setLife(3);
+									mForm.setScore(0);
+									flgPlay = false;
+									flgGameOver = true;
+								}
+							}
+						}
+						else {
+							//not catched
+							if (ballHead->next->data.isPie()) {
+								//missed pie
+								mForm.setScore(mForm.getScore() - 10);
+								mForm.scoreBox.refresh();
+							}
+						}
+						//destroy this ball
+						ballHead->next->data.hide();
+						ballNode* p = ballHead->next;
+						ballHead->next = p->next;
+						delete p;
+
+					}
+
+				}
+			}
+			
+
+
+		}
+		if (flgGameOver) {
+			//reset game
+			//destroy list
+			ballNode* p = ballHead;
+			while (p != NULL) {
+				p = p->next;
+				delete ballHead;
+				ballHead = p;
+			}
+			//new list
+			ballHead = new ballNode;
+			ballHead->next = NULL;
+			ballLast = ballHead;
+
+			mario.setPos(400, 465);
+			mForm.refresh();
+			btPlay.show();
+			btStop.show();
+			btExit.show();
+			mario.refresh();
+			flgGameOver = false;
+		}
+		//if just dead
+		if (flgDead) {
+			//death anim
+			while (mario.getY() > 400) {
+				mario.moveU(1);
+				Sleep(1);
+			}
+			while (mario.getY() < 900) {
+				BeginBatchDraw();
+				mario.moveD(2);
+				btPlay.show();
+				btStop.show();
+				btExit.show();
+				EndBatchDraw();
+				Sleep(1);
+			}
+			Sleep(1500);
+			timeGame = 0;
+			flgGameOver = true;
+			flgDead = false;
 		}
 
 	}
 
-
+	//destroy list
+	ballNode* p = ballHead;
+	while (p!= NULL) {
+		p = p->next;
+		delete ballHead;
+		ballHead = p;
+	}
 	//_getch();
 	closegraph();
 	return 0;
